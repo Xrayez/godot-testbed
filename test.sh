e0d0bdc7e29ea2f96c9dd08bbf66ee18856a01bc
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2019-10-05 22:33:02 +0300

    Implement running tests on headless server platform

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-godot --path testbed --no-window -s addons/gut/gut_cmdln.gd
+# For reference, see:
+# https://github.com/godotengine/godot-tests/blob/master/tests/project_export/test_project.sh
+
+# The absolute path to the Godot binary
+GODOT_BIN="$(readlink -e "$1")"
+
+# Change to the directory where the script is located,
+# so it can be run from any location
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+"$GODOT_BIN" --path testbed -s addons/gut/gut_cmdln.gd
