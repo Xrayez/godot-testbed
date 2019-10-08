bd04660a8c09c9fa5602b30584427c91c9735c70
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2019-10-08 14:13:30 +0300

    Better refine 32562 test
    For some reason this still fails on Windows:
    
    https://github.com/godotengine/godot/issues/32562#issuecomment-539227236
    
    So it can be considered to move this test as failing later...

--- testbed/tests/issues/32562/test_tilemap_fix_invalid_tiles.gd
+++ testbed/tests/issues/32562/test_tilemap_fix_invalid_tiles.gd
@@ -12,13 +12,17 @@ var map
 
 func setup():
 	map = TileMapFixInvalidTilesCrash.new()
+	# map.tile_set = TileSet.new() # crash doesn't happen if set
+	# map.tile_set = MeshLibrary.new() # throws no errors, tile_set == null
 	add_child(map)
 	
 	
 func test_fix_invalid_tiles():
 	yield(get_tree(), 'idle_frame')
-	assert_true(true, "No crash, great!")
 	
+	assert_eq(map.tile_set, null, "There should be no tile set assigned!")
+	assert_true(true, "No crash, great!")
 
+	
 func teardown():
 	map.queue_free()
